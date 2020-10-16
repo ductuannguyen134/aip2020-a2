8b9654545e8c32ed7d76ccd5796402ceaab2647f
Trung Duc <trungduc@Trungs-MacBook-Pro.local>
2020-10-16 16:32:30 +0700

    complete addRequest UI

--- frontend/src/components/FavorAdd/index.js
+++ frontend/src/components/FavorAdd/index.js
@@ -1,140 +1,141 @@
-import React, { useState } from 'react';
-import Select from '@material-ui/core/Select';
-import MenuItem from '@material-ui/core/MenuItem';
-import './style.css';
-import { Button, IconButton, Input } from '@material-ui/core';
-import AddIcon from '@material-ui/icons/Add';
-import RemoveIcon from '@material-ui/icons/Remove';
-import { useHistory } from 'react-router-dom';
-import axios from '../../hoc/axios';
+import React, { useState } from "react";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import "./style.css";
+import { Button, IconButton, Input } from "@material-ui/core";
+import AddIcon from "@material-ui/icons/Add";
+import RemoveIcon from "@material-ui/icons/Remove";
+import { useHistory } from "react-router-dom";
+import axios from "../../hoc/axios";
 
 function FavorAdd(props) {
-    const { handleAdd, onUpdate } = props
-    const [person, setPerson] = useState("");
-    const [inputList, setInputList] = useState([{ name: "", quantity: "" }]);
-    let history = useHistory();
-    
-    function handleChangePerson(e){
-        setPerson(e.target.value);
-    }
+  const { handleAdd, onUpdate } = props;
+  const [person, setPerson] = useState("");
+  const [inputList, setInputList] = useState([{ name: "", quantity: "" }]);
+  let history = useHistory();
 
-    function handleInputChange (e, index) {
-        const { name, value } = e.target;
-        const list = [...inputList];
-        list[index][name] = value;
-        setInputList(list);
-        };
-    
-    function handleRemoveInputField (index) {
+  function handleChangePerson(e) {
+    setPerson(e.target.value);
+  }
+
+  function handleInputChange(e, index) {
+    const { name, value } = e.target;
+    const list = [...inputList];
+    list[index][name] = value;
+    setInputList(list);
+  }
+
+  function handleRemoveInputField(index) {
     const list = [...inputList];
     list.splice(index, 1);
     setInputList(list);
-    };
-    
+  }
 
-    function handleAddInputField () {
+  function handleAddInputField() {
     setInputList([...inputList, { name: "", quantity: "" }]);
+  }
+
+  function cancel(e) {
+    history.push("/favors");
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    alert("Submitted!");
+    const favor = {
+      ownerID: "5f862b953d152a307c75cd05",
+      // debtorID: "5f864281a9213334cb6592ec",
+      debtorID: "5f86ec1cec72c8517e88b13d",
+      items: inputList,
     };
 
-    function cancel(e){
-        history.push('/favors');
-    }
-    
-    function handleSubmit(event) 
-    {
-        event.preventDefault();
-        alert("Submitted!");
-        const favor = {
-            ownerID: "5f862b953d152a307c75cd05",
-            // debtorID: "5f864281a9213334cb6592ec",
-            debtorID: "5f86ec1cec72c8517e88b13d",
-            items: inputList
-        };
-    
-        console.log(favor);
+    console.log(favor);
 
-        axios.post("/api/favor/create", favor)
-        .then(res => 
-            {
-                console.log(res.data);
-                handleAdd(favor);
-            })
-        .catch((err) => console.log(err));
-    }
+    axios
+      .post("/api/favor/create", favor)
+      .then((res) => {
+        console.log(res.data);
+        handleAdd(favor);
+      })
+      .catch((err) => console.log(err));
+  }
 
-    return (
-        <div className="favorAdd">
-            <h1>Add favor for you</h1>
-            <div className="favorAdd__body">
-                <div className="favorAdd__left">
-                    <div className="favorAdd__chooseFrom">
-                        <p>From: </p>
-                        <Select 
-                            id="choosePerson"
-                            value={person}
-                            onChange={handleChangePerson}
-                            style = {{width: '20ch'}}
-                        >
-                            <MenuItem value="Chris">Chris</MenuItem>
-                            <MenuItem value="Hailey">Hailey</MenuItem>
-                            <MenuItem value="Duc">Duc</MenuItem>
-                            <MenuItem value="Thinh">Thinh</MenuItem>
-                        </Select>
-                    </div>
+  return (
+    <div className="favorAdd">
+      <h1>Add favor for you</h1>
+      <div className="favorAdd__body">
+        <div className="favorAdd__left">
+          <div className="favorAdd__chooseFrom">
+            <p>From: </p>
+            <Select
+              id="choosePerson"
+              value={person}
+              onChange={handleChangePerson}
+              style={{ width: "20ch" }}
+            >
+              <MenuItem value="Chris">Chris</MenuItem>
+              <MenuItem value="Hailey">Hailey</MenuItem>
+              <MenuItem value="Duc">Duc</MenuItem>
+              <MenuItem value="Thinh">Thinh</MenuItem>
+            </Select>
+          </div>
 
-                    {inputList.map((item, index) => {
-                         return (
-                    <div>
-                        <div className="favorAdd__chooseItems">
-                            <p>Items: </p>
-                            <Select
-                                id="chooseItem"
-                                name="name"
-                                value={item.name}
-                                onChange={(e)=>handleInputChange(e, index)}
-                            >
-                                <MenuItem value="Chocolate">Chocolate</MenuItem>
-                                <MenuItem value="Coffee">Coffee</MenuItem>
-                                <MenuItem value="Candy">Candy</MenuItem>
-                                <MenuItem value="Tea">Tea</MenuItem>
-                            </Select>
+          {inputList.map((item, index) => {
+            return (
+              <div>
+                <div className="favorAdd__chooseItems">
+                  <p>Items: </p>
+                  <Select
+                    id="chooseItem"
+                    name="name"
+                    value={item.name}
+                    onChange={(e) => handleInputChange(e, index)}
+                  >
+                    <MenuItem value="Chocolate">Chocolate</MenuItem>
+                    <MenuItem value="Coffee">Coffee</MenuItem>
+                    <MenuItem value="Candy">Candy</MenuItem>
+                    <MenuItem value="Tea">Tea</MenuItem>
+                  </Select>
 
-                            <Input
-                                className="favorAdd__itemNumber"
-                                name="quantity"
-                                value={item.quantity}
-                                onChange={(e)=>handleInputChange(e, index)}
-                                inputProps={{
-                                    type: 'number',
-                                    min: 0,
-                                    max: 100,
-                                }}
-                            />
-                            {inputList.length !== 1 && <IconButton onClick={()=>handleRemoveInputField(index)}>
-                                <RemoveIcon />
-                            </IconButton>}
-                        </div>
-                        {inputList.length - 1 === index && <IconButton onClick={handleAddInputField}>
-                            <AddIcon />
-                            </IconButton> }
-
-                    </div>
-                         );
-                          })}
+                  <Input
+                    className="favorAdd__itemNumber"
+                    name="quantity"
+                    value={item.quantity}
+                    onChange={(e) => handleInputChange(e, index)}
+                    inputProps={{
+                      type: "number",
+                      min: 0,
+                      max: 100,
+                    }}
+                  />
+                  {inputList.length !== 1 && (
+                    <IconButton onClick={() => handleRemoveInputField(index)}>
+                      <RemoveIcon />
+                    </IconButton>
+                  )}
                 </div>
-                <div className="favorAdd__right">
-                    <div className="favorAdd__proof">
-                        <p>Proof Required</p>
-                        <Input inputProps={{type: 'file'}}/>
-                    </div>
-                    <div className="favorAdd__buttons">
-                        <Button onClick={handleSubmit}>Create Favor</Button>
-                        <Button onClick={cancel}>Close</Button>
-                    </div>
-                </div>
-            </div>
+                {inputList.length - 1 === index && (
+                  <IconButton onClick={handleAddInputField}>
+                    <AddIcon />
+                  </IconButton>
+                )}
+              </div>
+            );
+          })}
         </div>
-    )
+        <div className="favorAdd__right">
+          <div className="favorAdd__proof">
+            <p>Proof Required</p>
+            <Input inputProps={{ type: "file" }} />
+          </div>
+          <div className="favorAdd__buttons">
+            <Button onClick={handleSubmit}>Create Favor</Button>
+            <Button onClick={cancel}>Close</Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default FavorAdd
+export default FavorAdd;
--- frontend/src/components/RequestAdd/index.js
+++ frontend/src/components/RequestAdd/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useReducer } from "react";
 import "./styles.css";
 import Select from "@material-ui/core/Select";
 import MenuItem from "@material-ui/core/MenuItem";
@@ -10,29 +10,51 @@ import axios from "../../hoc/axios";
 import { useUserStatus } from "../../hoc/UserContext";
 
 function RequestAdd() {
-  const [person, setPerson] = useState();
-  const [item, setItem] = useState();
-  const [itemNum, setItemNum] = useState();
   const [{ user }, dispatch] = useUserStatus();
   const [request, setRequest] = useState();
-  const [users, setUsers] = useState([]);
+  const [prizes, setPrize] = useState([]);
+  const [items, setItems] = useState([]);
 
   useEffect(() => {
-    const getUserList = async () => {
-      const response = await axios.get("/api/user/users");
-      const users = response.data;
+    const getPrizeList = async () => {
+      const res = await axios.get("/api/prize/");
+      const prizes = res.data;
 
-      setUsers(users)
-    }
-    
-    getUserList()
+      setPrize(prizes);
+    };
+
+    getPrizeList();
   }, []);
 
-  const history = useHistory();
+  const handleAddItem = () => {
+    if (items.length < prizes.length) {
+      setItems([...items, { id: "", quantity: 0 }]);
+    } else {
+      alert("Cant add more type of reward");
+    }
+  };
+
+  const handleChangeItem = (item, index) => {
+    const list = [...items];
+    list[index].id = item;
+    setItems(list);
+  };
+
+  const handleChangeItemNum = (quantity, index) => {
+    const list = [...items];
+    list[index].quantity = quantity;
+    setItems(list);
+  };
+
+  const handleRemoveItem = (index) => {
+    const list = [...items];
+    list.splice(index, 1);
+    setItems(list);
+  };
 
   const addRequest = async () => {
     if (user) {
-      if (!item || !itemNum || !request) {
+      if (!items || !request) {
         alert("All fields must be filled in!");
       } else {
         await axios
@@ -43,16 +65,11 @@ function RequestAdd() {
               requestFavors: [
                 {
                   from: user.userID,
-                  rewards: [
-                    {
-                      name: item,
-                      quantity: itemNum,
-                    },
-                  ],
+                  rewards: [...items],
                 },
               ],
-              resolverID: "5f76c9967785b0887cefb148",
-              resolverProof: "Proven",
+              resolverID: null,
+              resolverProof: null,
             },
             {
               headers: {
@@ -69,7 +86,7 @@ function RequestAdd() {
           });
       }
     } else {
-      alert("You must log in to add a request!");
+      alert("You must log in again to add a request!");
     }
   };
 
@@ -79,8 +96,9 @@ function RequestAdd() {
         <h1>Add a new request</h1>
         <div className="req-dialog-request">
           <div className="req-dialog-request text">
-            <p>Request:</p>
-            <span>(Required)</span>
+            <p>
+              Request: <span style={{ color: "red" }}>(Required)</span>
+            </p>
           </div>
           <div className="req-dialog-request-input">
             <textarea
@@ -93,55 +111,55 @@ function RequestAdd() {
         </div>
         <div className="req-dialog-reward">
           <div className="req-dialog-reward text">
-            <p>Rewards:</p>
-            <span>(Required)</span>
+            <p>
+              Rewards: <span style={{ color: "red" }}>(Required)</span>
+            </p>
           </div>
           <div className="widgets">
-            <div className="req-choosefrom">
-              <p>From: </p>
-              <Select
-                id="choosePerson"
-                value={person}
-                onChange={(e) => setPerson(e.target.value)}
-                style={{ width: "20ch" }}
-              >
-                {users.map(user => (
-                    <MenuItem value={user["_id"]}>{user["userName"]}</MenuItem>
-                ))}
-              </Select>
+            <p>Items: </p>
+            <div>
+              {items.map((item, index) => (
+                <div
+                  className="req-chooseitem"
+                  key={item.id != null ? item.id : items.length}
+                >
+                  <Select
+                    id="chooseItem"
+                    value={item.id}
+                    onChange={(e) => handleChangeItem(e.target.value, index)}
+                  >
+                    {prizes.map((prize) => (
+                      <MenuItem value={prize["_id"]}>{prize["prize"]}</MenuItem>
+                    ))}
+                  </Select>
+                  <Input
+                    style={{ minWidth: "10px" }}
+                    className="req-chooseItemNum"
+                    value={item.quantity}
+                    onChange={(e) => handleChangeItemNum(e.target.value, index)}
+                    inputProps={{
+                      type: "number",
+                      min: 0,
+                      max: 10,
+                    }}
+                  />
+                  <IconButton onClick={() => handleRemoveItem(index)}>
+                    <RemoveIcon />
+                  </IconButton>
+                </div>
+              ))}
             </div>
-            <div className="req-chooseitem">
-              <p>Items: </p>
-              <Select
-                id="chooseItem"
-                value={item}
-                onChange={(e) => setItem(e.target.value)}
-              >
-                <MenuItem value="Chocolate">Chocolate</MenuItem>
-                <MenuItem value="Coffee">Coffee</MenuItem>
-                <MenuItem value="Candy">Candy</MenuItem>
-                <MenuItem value="Tea">Tea</MenuItem>
-              </Select>
-              <Input
-                className="req-chooseItemNum"
-                onChange={(e) => setItemNum(e.target.value)}
-                inputProps={{
-                  type: "number",
-                  min: 0,
-                  max: 100,
-                }}
-              />
-              <IconButton onClick={() => alert("Remove")}>
-                <RemoveIcon />
+            {items.length == prizes.length ? null : (
+              <IconButton onClick={handleAddItem}>
+                <AddIcon />
               </IconButton>
-            </div>
-            <IconButton onClick={() => alert("add")}>
-              <AddIcon />
-            </IconButton>
+            )}
           </div>
         </div>
         <div className="req-button">
-          <Button onClick={addRequest}>Create Request</Button>
+          <Button onClick={addRequest} color="primary" variant="contained">
+            Create Request
+          </Button>
         </div>
       </div>
     </div>
