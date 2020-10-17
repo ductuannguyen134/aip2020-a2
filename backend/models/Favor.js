c3eb6ebe71f24fd6fbbd6b6928ec4783c4011cb0
Trung Duc <trungduc@Trungs-MacBook-Pro.local>
2020-10-17 12:59:52 +0700

    edit Favor model

--- backend/models/Favor.js
+++ backend/models/Favor.js
@@ -1,44 +1,40 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const favorModel = {
-    ownerID: {
+  ownerID: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  debtorID: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  items: [
+    {
+      id: {
         type: Schema.Types.ObjectId,
-        ref: 'User',
+        ref: "Prize",
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+      },
     },
-    debtorID: {
-        type: Schema.Types.ObjectId,
-        ref: 'User',
-    },
-    items: [{
-        name: {
-            type: String,
-            required: true,
-        },
-        quantity: {
-            type: Number,
-            required: true,
-        },
-    }],
-    isComplete: {
-        type: Boolean,
-        default: false,
-    },
-    createdImage: {
-        type: String,
-    },
-    completedImage: {
-        type: String,
-    },
-    completedTime: {
-        type: Schema.Types.Date,
-    },
-//      photo: {
-//     data: Buffer,
-//     contentType: String
-//   }
-}
+  ],
+  isComplete: {
+    type: Boolean,
+    default: false,
+  },
+  createdImage: {
+    type: String,
+  },
+  completedImage: {
+    type: String,
+  },
+};
 
-const FavorSchema = new mongoose.Schema(favorModel, {timestamps: true});
+const FavorSchema = new mongoose.Schema(favorModel, { timestamps: true });
 
-module.exports = mongoose.model('Favor', FavorSchema, "favors");
+module.exports = mongoose.model("Favor", FavorSchema, "favors");
